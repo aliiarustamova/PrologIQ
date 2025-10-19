e8301aa03bf83f1c50727471f43d792c722c670f
Viplav Dodeja <viplav.dodeja@gmail.com>
2025-10-19 12:32:07 -0700

    firebase config file

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Z9fv06jKfEx_UXUA90cPRBWV4jwS3oA",
+  authDomain: "prologiq-hub-a08da.firebaseapp.com",
+  projectId: "prologiq-hub-a08da",
+  storageBucket: "prologiq-hub-a08da.firebasestorage.app",
+  messagingSenderId: "271479122521",
+  appId: "1:271479122521:web:8170dc1042d3c8c5ff5f48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
